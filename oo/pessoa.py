8de4237d3fe74e29fe87876a8ca1d114ed4be14c
ricardozattera <ricardo.zattera@icloud.com>
2020-09-07 16:54:06 -0300

    Adicionado atributos complexos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,18 @@
 class Pessoa:
-    pass
+    def __init__(self, *filhos, nome=None, idade=35):
+        self.idade = idade
+        self.nome = nome
+        self.filhos = list(filhos)
+
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
+
+if __name__ == '__main__':
+    brenda = Pessoa(nome='Brenda', idade=26)
+    ricardo = Pessoa(brenda,nome='Ricardo', idade=48)
+    print(ricardo.cumprimentar())
+    print(ricardo.nome)
+    print(ricardo.idade)
+    for filho in ricardo.filhos:
+        print(filho.nome)
+        print(filho.idade)
